14d8b9c1ee9f6adf9f4c1e9d82bef126b195e8fe
zzx2GH <zzx2wy@163.com>
2017-09-14 00:11:14 +0800

    Add files via upload

---
+++ new.py
@@ -0,0 +1,114 @@
+import serial
+import time
+import datetime
+import sys
+import paddle.v2 as paddle
+from PIL import Image
+import numpy as np
+import cv2
+from vgg import vgg_bn_drop
+
+
+def multilayer_perceptron(img):
+	hidden1 = paddle.layer.fc(input=img, size=128, act=paddle.activation.Relu())
+	hidden2 = paddle.layer.fc(input=hidden1, size=64, act=paddle.activation.Relu())
+	predict = paddle.layer.fc(input=hidden2, size=5, act=paddle.activation.Softmax())
+	return predict
+
+camera_port = 0
+ramp_frames = 1
+camera = cv2.VideoCapture(camera_port)
+
+camera.set(3,320)
+camera.set(4,240)
+
+def get_image():
+    retval, im = camera.read()
+    return im
+ 
+ser = serial.Serial('/dev/cu.usbserial-FT9TLFUX', 9600, timeout=0.001)
+words = '1'
+
+
+for i in range(ramp_frames):
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+    temp = get_image()
+
+datadim = 3 * 320 * 240
+classdim = 5
+
+# PaddlePaddle init
+paddle.init(use_gpu=False, trainer_count=1)
+
+image = paddle.layer.data(
+    name="image", type=paddle.data_type.dense_vector(datadim))
+
+net = multilayer_perceptron(image)
+       
+out = paddle.layer.fc(
+    input=net, size=classdim, act=paddle.activation.Softmax())
+
+lbl = paddle.layer.data(
+    name="label", type=paddle.data_type.integer_value(classdim))
+cost = paddle.layer.classification_cost(input=out, label=lbl)
+
+with open('params_pass_46.tar', 'r') as f:
+        parameters = paddle.parameters.Parameters.from_tar(f)
+j = 0
+while(1):
+    line = ser.readline().decode()
+    #print line
+    #print(line)
+    if line == '6':
+        print("Receive "+line)
+            #       for i in range(ramp_frames):
+            #print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+            #temp = get_image()
+        #print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+        #print("Taking image...")
+        camera_capture = get_image()
+        #print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+
+            #print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+        file = './'+str(j)+'.png'
+        cv2.imwrite(file, camera_capture)
+        j = j+1
+
+
+        im = np.array(camera_capture).astype(np.float32)
+
+        im = im.transpose((2, 0, 1))  # CHW
+
+        im = im.flatten()
+        im = im / 255.0
+
+
+        test_data = []
+        test_data.append((im,))
+
+
+        probs = paddle.infer(
+            output_layer=out, parameters=parameters, input=test_data)
+        lab = np.argsort(-probs)  # probs and lab are the results of one batch data
+        #print "Label of image/dog.png is: %d" % lab[0][0]
+
+
+
+        ser.write(str(lab[0][0]))
+        print(str(lab[0][0]))
+        print probs[0][lab[0][0]]
+        #if words=="1":
+        #    words="2"
+        #elif words=="2":
+        #    words="3"
+        #elif words=="3":
+        #    words="4"
+        #elif words=="4":
+        #    words="5"
+        #elif words=="5":
+        #    words="1"
+
+ser.close()
+
+ 
+del(camera)
